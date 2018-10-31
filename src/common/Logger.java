d4f6806290e6dea2384feb4b1ec385c5a67de1ae
Jorge Jiménez <jrg.jmnz.prd@gmail.com>
2018-10-31 11:37:21 -0500

    HALLOWEEN

--- src/common/Logger.java
+++ src/common/Logger.java
@@ -18,11 +18,24 @@ public class Logger {
 			File f = new File(nombreDirectorio + LocalDate.now() + ".txt");
 			FileWriter fw = new FileWriter(f, true);
 			BufferedWriter bw = new BufferedWriter(fw);
-			bw.write("FECHA-HORA: " + LocalDateTime.now());
-			bw.write("LOCALIZACIÓN: " + e.getStackTrace()[2].getMethodName());
-			bw.write("EXCEPCIÓN: " + e.getClass().getName());
-			bw.write("DETALLE: " + e.getMessage());
 			bw.newLine();
+			bw.write("FECHA-HORA: " + LocalDateTime.now());
+			bw.newLine();
+			bw.write("EXCEPCIÓN: " + e.getClass().getName() + " - " + e.getMessage());
+			bw.newLine();
+			bw.write("DETALLE: ");
+			bw.newLine();
+			for (int i=0; i < 5 ; i++) {
+				bw.write("ARCHIVO: " + e.getStackTrace()[i].getFileName());
+				bw.newLine();
+				bw.write("\t LÍNEA: " + e.getStackTrace()[i].getLineNumber());
+				bw.newLine();
+				bw.write("\t \t CLASE: " + e.getStackTrace()[i].getClassName());
+				bw.newLine();
+				bw.write("\t \t \t MÉTODO: " + e.getStackTrace()[i].getMethodName());
+				bw.newLine();
+			}
+			bw.write("***********************************************************************");
 			bw.flush();
 			bw.close();
 		} catch (Exception ex) {
